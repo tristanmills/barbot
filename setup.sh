23c9c716ad5bdc86f93e50538abb322975f4d49a
tristanmills <tristanmills11@gmail.com>
2018-05-15 16:29:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,28 @@
 #!/bin/sh
+
+apt-get -y update
+apt-get -y upgrade
+
+apt-get -y install git
+
+apt-get -y install apache2
+
+apt-get -y install python3-pip
+apt-get -y install libapache2-mod-wsgi-py3
+pip3 install Flask
+
+# sudo apt-get -y install python-pip
+# sudo apt-get -y install libapache2-mod-wsgi
+# sudo pip install Flask
+
+git clone https://github.com/tristanmills/barbot.git /var/www/
+
+ln -s /var/www/barbot/barbot.conf /etc/apache2/sites-available/barbot.conf
+a2ensite barbot.conf
+a2dissite default
+rm -rf /var/www/html/
+systemctl reload apache2
+
+hostnamectl set-hostname barbot
+
 exit 0
